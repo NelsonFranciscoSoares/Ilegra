33580a53147bb691a11260b36d4ad737b8fba3d5
Nelson Francisco Vicente Soares <nelsonfranciscosoares1987@gmail.com>
2016-04-22 10:31:09 -0300

    Added Visual Studio Solution

---
+++ Ilegra/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilegra
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
